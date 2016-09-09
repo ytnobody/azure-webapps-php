3e7406c14526484a4158a67301cb6ff59000b862
ytnobody <ytnobody@gmail.com>
2016-09-09 12:26:22 +0900

    using json response

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $app = new Slim\App();
 
 $app->get('/hello/{name}', function ($req, $res, $arg) {
-    $res->write('Hello, '. $arg['name']);
+    $res->withJson(array(message => 'Hello, '. $arg['name']));
     return $res;
 });
 

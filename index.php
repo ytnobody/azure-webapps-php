a7c7f76f7fe002d4f7668b617b435eb5be725b51
ytnobody <ytnobody@gmail.com>
2016-09-09 12:46:17 +0900

    for matching sequence

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ require 'vendor/autoload.php';
 
 $app = new Slim\App();
 
-### redirect to /hello/azure_webapps
-$app->get('/', function ($req, $res, $arg) {
-    return $res->withStatus(302)
-               ->withHeader('Location', '/hello/azure_webapps');
-});
-
 ### output json
 $app->get('/hello/{name}', function ($req, $res, $arg) {
     $data = array(message => 'Hello, '. $arg['name']);
     $res->withJson($data);
     return $res;
+});
+
+### redirect to /hello/azure_webapps
+$app->get('/', function ($req, $res, $arg) {
+    return $res->withStatus(302)
+               ->withHeader('Location', '/hello/azure_webapps');
 });
 
 $app->run();
